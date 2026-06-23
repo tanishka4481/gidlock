db1f4466d17a6f8ea8709b3bcbdeae895afbe18c
tanishka4481 <tanishkasharmaa.3183@gmail.com>
2026-06-23 23:42:16 +0530

    prep for render deployment

--- server.py
+++ server.py
@@ -396,4 +396,5 @@ def calculate_routes():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
